7343978b2892e77a7e6b9c5cf1945590ce529df5
LeeJiEun <gsilverg197@gmail.com>
2024-09-12 13:47:36 +0900

    🎉 Init: set up DB connection parameters

--- src/main/java/com/ssg/secondProject/SecondProjectApplication.java
+++ src/main/java/com/ssg/secondProject/SecondProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecondProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SecondProjectApplication.class, args);
     }
-
 }
