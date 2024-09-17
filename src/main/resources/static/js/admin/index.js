d7b948b3e0eeb3e85c48d8db5eb18b74af8486fd
LeeJiEun <gsilverg197@gmail.com>
2024-09-18 02:25:09 +0900

    ✨ Feat: Add search box on admin index page

---
+++ src/main/resources/static/js/admin/index.js
@@ -0,0 +1,54 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   let select = document.querySelector(".select");
+//   let optionList = document.querySelector(".option-list");
+//   let options = document.querySelectorAll(".option");
+//
+//   select.addEventListener("click", () => {
+//     optionList.classList.toggle("active");
+//   });
+//
+//   options.forEach((option) => {
+//     option.addEventListener("click", () => {
+//       options.forEach((option) => {
+//         option.classList.remove('selected');
+//       });
+//       select.querySelector("span").innerText = option.innerHTML;
+//       option.classList.add("selected");
+//     });
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let selects = document.querySelectorAll(".select");
+  let optionLists = document.querySelectorAll(".option-list");
+
+  selects.forEach((select, index) => {
+    let optionList = optionLists[index];
+    let options = optionList.querySelectorAll(".option");
+
+    select.addEventListener("click", () => {
+      optionList.classList.toggle("active");
+    });
+
+    options.forEach((option) => {
+      option.addEventListener("click", () => {
+        options.forEach((opt) => {
+          opt.classList.remove('selected');
+        });
+
+        select.querySelector("span").innerText = option.innerHTML;
+        option.classList.add("selected");
+
+        optionList.classList.remove("active");
+      });
+    });
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!event.target.closest(".select-menu")) {
+      optionLists.forEach(optionList => {
+        optionList.classList.remove("active");
+      });
+    }
+  });
+});
