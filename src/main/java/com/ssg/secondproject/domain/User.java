dccc2ddfee9640a8523ba8712854a50734794277
LeeJiEun <gsilverg197@gmail.com>
2024-09-25 14:21:23 +0900

    🐛 Fix: Correct MyBatis configuration issue in development environment

--- src/main/java/com/ssg/secondproject/domain/User.java
+++ src/main/java/com/ssg/secondproject/domain/User.java
@@ -2,6 +2,5 @@ package com.ssg.secondproject.domain;
 
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public class User {
 }
--- src/main/java/com/ssg/secondproject/mapper/UserMapper.java
+++ src/main/java/com/ssg/secondproject/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.ssg.secondproject.mapper;
 
 import com.ssg.secondproject.domain.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper {
 
     User findAll();
