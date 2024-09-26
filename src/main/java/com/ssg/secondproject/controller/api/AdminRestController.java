cf11ce9c4b5ec2852961ba2dea2868bf2171943c
LeeJiEun <gsilverg197@gmail.com>
2024-09-26 18:52:44 +0900

    ✨ Feat: Implement restController to get admin detail info

--- src/main/java/com/ssg/secondproject/controller/api/AdminRestController.java
+++ src/main/java/com/ssg/secondproject/controller/api/AdminRestController.java
@@ -3,6 +3,7 @@ package com.ssg.secondproject.controller.api;
 import com.ssg.secondproject.dto.request.PageRequestDTO;
 import com.ssg.secondproject.dto.response.AdminResponseDTO;
 import com.ssg.secondproject.dto.response.PageListResponseDTO;
+import com.ssg.secondproject.dto.response.PageResponseDTO;
 import com.ssg.secondproject.service.AdminService;
 import java.nio.charset.Charset;
 import lombok.RequiredArgsConstructor;
@@ -23,40 +24,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminRestController {
 
-    private HttpHeaders httpHeaders = new HttpHeaders();
     private final AdminService adminService;
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        return httpHeaders;
+    }
 
     @GetMapping("/list")
     public ResponseEntity<PageListResponseDTO<AdminResponseDTO>> getAdminList(PageRequestDTO pageRequestDTO, BindingResult bindingResult) {
         log.info(" 🎉🎉🎉🎉 Rest Admin List 🎉🎉🎉🎉 ");
+//
         PageListResponseDTO<AdminResponseDTO> data =  adminService.getList(pageRequestDTO);
-        httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
-        return new ResponseEntity<>(data, httpHeaders, HttpStatus.OK);
-//        return new ResponseEntity<>(message, httpHeaders, HttpStatus.OK);
+        return new ResponseEntity<>(data, getHttpHeaders(), HttpStatus.OK);
     }
 
     @GetMapping()
-    public String getAdmin(@RequestParam int id) {
+    public ResponseEntity<PageResponseDTO<AdminResponseDTO>> getAdmin(@RequestParam int id) {
         log.info(" 🎉🎉🎉🎉 Rest Admin Detail 🎉🎉🎉🎉\n ID:  " + id);
-        return """
-            {
-                "status": "success",
-                "data": {
-                     "admin": {
-                          "id": 1,
-                          "name": "신짱구",
-                          "adminId" : "jjanggu",
-                          "email": "jjanggu@3pl.co.kr",
-                          "role": "총관리자",
-                          "department": "개발팀",
-                          "position": "총괄팀장",
-                          "phone": "010-2734-5912",
-                          "createdAt": "2022-09-15 12:30:45",
-                          "updatedAt": "2022-09-30 09:25:12"
-                        }
-                }
-            }
-            """;
+
+        PageResponseDTO<AdminResponseDTO> data = adminService.getById(id);
+
+        return new ResponseEntity<>(data, getHttpHeaders(), HttpStatus.OK);
     }
 }
