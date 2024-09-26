a6670703bae8559bed2d5bbf499152bb6b61db89
LeeJiEun <gsilverg197@gmail.com>
2024-09-27 03:16:24 +0900

    ✨ Feat: Implement mapper to update admin info

---
+++ src/main/java/com/ssg/secondproject/dto/request/AdminRequestDTO.java
@@ -0,0 +1,24 @@
+package com.ssg.secondproject.dto.request;
+
+import com.ssg.secondproject.common.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminRequestDTO {
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private int warehouseId;
+    private Role role;
+    private String phone;
+    private int updatedId;
+    private String updatedAt;
+
+}
--- src/main/java/com/ssg/secondproject/mapper/AdminMapper.java
+++ src/main/java/com/ssg/secondproject/mapper/AdminMapper.java
@@ -1,6 +1,7 @@
 package com.ssg.secondproject.mapper;
 
 import com.ssg.secondproject.domain.Admin;
+import com.ssg.secondproject.dto.request.AdminRequestDTO;
 import com.ssg.secondproject.dto.request.PageRequestDTO;
 import com.ssg.secondproject.dto.response.AdminResponseDTO;
 import java.util.List;
@@ -9,10 +10,12 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface AdminMapper {
 
-    int getAdminCnt(PageRequestDTO pageRequestDTO);
+    int getAdminCnt(PageRequestDTO requestDTO);
 
-    List<Admin> findAll(PageRequestDTO pageRequestDTO);
+    List<Admin> findAll(PageRequestDTO requestDTO);
 
     AdminResponseDTO findById(int id);
+
+    void updateById(AdminRequestDTO requestDTO);
 
 }
