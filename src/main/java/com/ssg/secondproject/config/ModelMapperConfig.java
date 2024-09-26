f3f0ccc58ec7f66a2e07f2d0222297e9076d6200
LeeJiEun <gsilverg197@gmail.com>
2024-09-26 15:16:11 +0900

    Revert "✨ Feat: Add ModelMapper converter for LocalDateTime to String transformation"
    This reverts commit f8d5b2f2f299b90b33a8a1a05c5b174911d9f173.

--- src/main/java/com/ssg/secondproject/config/ModelMapperConfig.java
+++ src/main/java/com/ssg/secondproject/config/ModelMapperConfig.java
@@ -1,12 +1,7 @@
 package com.ssg.secondproject.config;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,25 +11,10 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper getMapper() {
         ModelMapper modelMapper = new ModelMapper();
-
         modelMapper.getConfiguration()
             .setFieldMatchingEnabled(true)
-            .setFieldAccessLevel(AccessLevel.PRIVATE)
+            .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
             .setMatchingStrategy(MatchingStrategies.LOOSE);
-
-        Converter<LocalDateTime, String> toStringConverter =
-            new Converter<LocalDateTime, String>() {
-                public String convert(MappingContext<LocalDateTime, String> context) {
-                    if (context.getSource() == null) {
-                        return null;
-                    }
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
-                        "yyyy-MM-dd HH:mm:ss");
-                    return context.getSource().format(formatter);
-                }
-            };
-
-        modelMapper.addConverter(toStringConverter);
 
         return modelMapper;
     }
