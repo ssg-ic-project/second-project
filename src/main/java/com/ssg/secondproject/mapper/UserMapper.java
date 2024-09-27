ad70ad6572963964c6e02e5014e84a548f5c4c5b
LeeJiEun <gsilverg197@gmail.com>
2024-09-27 16:15:45 +0900

    ✨Feat: Implement getting user approval list by userId

--- src/main/java/com/ssg/secondproject/mapper/UserMapper.java
+++ src/main/java/com/ssg/secondproject/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.ssg.secondproject.mapper;
 import com.ssg.secondproject.domain.User;
 import com.ssg.secondproject.domain.UserApproval;
 import com.ssg.secondproject.dto.request.PageRequestDTO;
+import com.ssg.secondproject.dto.response.UserApprovalResponseDTO;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -15,6 +16,8 @@ public interface UserMapper {
 
     User findById(int id);
 
-    List<UserApproval> findApprovalByUserId(int userId);
+    int getApprovalCnt(int userId);
+
+    List<UserApprovalResponseDTO> findApprovalByUserId(int userId);
 
 }
--- src/main/java/com/ssg/secondproject/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/ssg/secondproject/service/serviceImpl/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.ssg.secondproject.service.serviceImpl;
 
 import com.ssg.secondproject.domain.User;
+import com.ssg.secondproject.domain.UserApproval;
 import com.ssg.secondproject.dto.PageInfoDTO;
 import com.ssg.secondproject.dto.request.PageRequestDTO;
 import com.ssg.secondproject.dto.response.PageListResponseDTO;
 import com.ssg.secondproject.dto.response.PageResponseDTO;
+import com.ssg.secondproject.dto.response.UserApprovalResponseDTO;
 import com.ssg.secondproject.dto.response.UserResponseDTO;
 import com.ssg.secondproject.mapper.UserMapper;
 import com.ssg.secondproject.service.UserService;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -26,8 +29,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public PageListResponseDTO<UserResponseDTO> getList(PageRequestDTO pageRequestDTO) {
         int total = userMapper.getUserCnt(pageRequestDTO);
-        log.info("total: " + total);
-        log.info("pageRequestDTO: " + pageRequestDTO.toString());
 
         List<User> voList = userMapper.findAll(pageRequestDTO);
         List<UserResponseDTO> dtoList = voList.stream()
@@ -58,5 +59,27 @@ public class UserServiceImpl implements UserService {
         PageResponseDTO<UserResponseDTO> responseDTO = PageResponseDTO.<UserResponseDTO>builder().data(userResponseDTO).build();
 
         return responseDTO;
+    }
+
+    @Override
+    public PageListResponseDTO<UserApprovalResponseDTO> getApprovalByUserId(int userId) {
+//        int total = userMapper.getApprovalCnt(userId);
+
+//        List<UserApproval> voList = userMapper.findApprovalByUserId(userId);
+
+//        List<UserApprovalResponseDTO> dtoList = userMapper.findApprovalByUserId(userId);
+
+//        PageInfoDTO pageInfoDTO =
+//            PageInfoDTO.withAll()
+//                .total(total)
+//                .build();
+
+//        PageListResponseDTO<UserApprovalResponseDTO> pageResponseDTO =
+//            PageListResponseDTO.builder()
+////                .pageInfoDTO(pageInfoDTO)
+//                .dataList(Collections.singletonList(dtoList))
+//                .build();
+
+        return null;
     }
 }
